0101766a59b09da35f3b365a49df3df6ab82bcb4
Mark <retrograde1779@gmail.com>
2026-04-20 12:17:44 +0300

    Коммит: создал генератор мемов, который обращается к открытому API и генерирует изображение. Сделал спиннер загрузки при ожидании. Сделал искусственное ожидание ответа от сервера через setTimeout для показа работы loaderа. Обращение сделал через fetchAPI

---
+++ memesGen/script.js
@@ -0,0 +1,42 @@
+async function getMemes(){
+    const res = await fetch('https://api.imgflip.com/get_memes')
+    return await res.json()
+}
+
+const loader = document.getElementById('loader')
+const content = document.getElementById('content')
+const button = document.getElementById('meme_btn')
+
+button.addEventListener('click', ev => {
+    // fetch('https://api.imgflip.com/get_memes')
+    //     .then(res => {
+    //         return res.json()
+    //     })
+    //     .then((data) => {
+    //         console.log(data)
+    //         document.getElementById('meme_img').setAttribute('src', data.data.memes[Math.floor(Math.random() * 100)].url)
+    //     })
+
+
+
+    getMemes()
+        .then((data) => {
+            let rand = Math.floor(Math.random() * 100)
+
+            button.disabled = true
+            content.style.display = 'none'
+            loader.style.display = 'block'
+
+            setTimeout(() => {
+                content.style.display = 'block'
+                content.setAttribute('src', data.data.memes[rand].url)
+                loader.style.display = 'none'
+                button.disabled = false
+            }, 3500)
+        })
+        .catch((err) => {
+            content.innerHTML = '<p style="color: red;">Произошла ошибка доступа к серверу</p>'
+        })
+        
+})
+
